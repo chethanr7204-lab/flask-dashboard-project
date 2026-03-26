cb91cfe24b10c21bdaa8680084ea7a032570b420
Chethan R <chethanr7204@gmail.com>
2026-03-26 21:38:52 +0530

    fix render port

--- app.py
+++ app.py
@@ -68,5 +68,9 @@ def logout():
     return redirect("/")
 
 # ---------------- RUN ----------------
+import os
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
